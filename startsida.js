12ef4df45c3c6c0bd11cf2ad9e491f1c57bd9b21
Walter Sjöblom <walter.sjoblom@skola.taby.se>
2026-03-17 08:31:35 +0100

    first commit

---
+++ startsida.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function(){
+if(window.scrollY > 50){
+header.classList.add("scrolled");
+}
+else{
+header.classList.remove("scrolled");
+}
+});
